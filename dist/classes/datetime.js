09804ccc282981ae43368807090a855470ff4d42
Nerwyn Singh <nerwyn1994@gmail.com>
2024-09-12 00:22:51 -0400

    disable webpack, removes export

---
+++ dist/classes/datetime.js
@@ -0,0 +1,112 @@
+import * as d3TimeFormat from 'd3-time-format';
+import { PyDate, PyTime } from '.';
+import { DatetimeIntervals, } from '../models';
+export class PyDatetime {
+    constructor(year, month, day, hour, minute, second, millisecond, utc) {
+        let args = {};
+        this.utc = utc;
+        if (typeof year == 'number' && !month && !day) {
+            // while a dt.datetime(2020) is perfectly valid, it's quite unlikely.
+            // much more unlikely than having gotten an epoch passed in. convert that to date
+            year = new Date(year);
+        }
+        if (year?.year &&
+            year?.month &&
+            year?.day) {
+            const ts = year;
+            DatetimeIntervals.forEach((field) => {
+                args[field] = ts[field];
+            });
+            if (ts.utc) {
+                args.utc = ts.utc;
+            }
+        }
+        else if (year instanceof Date) {
+            const ts = year;
+            args = {
+                year: ts.getFullYear(),
+                month: ts.getMonth() + 1,
+                day: ts.getDate(),
+                hour: ts.getHours(),
+                minute: ts.getMinutes(),
+                second: ts.getSeconds(),
+                millisecond: ts.getMilliseconds(),
+            };
+        }
+        else {
+            args = {
+                year: year,
+                month,
+                day,
+                hour,
+                minute,
+                second,
+                millisecond,
+            };
+        }
+        Object.assign(this, args);
+    }
+    replace(year, month, day, hour, minute, second, millisecond) {
+        // returns new date with updated values
+        let args = {};
+        if (year && typeof year != 'number') {
+            args = year;
+        }
+        else {
+            args = { year, month, day, hour, minute, second, millisecond };
+        }
+        const newTs = new PyDatetime(this);
+        Object.entries(args).forEach(([key, val]) => {
+            if (val) {
+                newTs[key] = val;
+            }
+        });
+        return newTs;
+    }
+    get jsDate() {
+        if (this.utc) {
+            return new Date(this.valueOf());
+        }
+        else {
+            return new Date(this.year, this.month - 1, this.day || 1, this.hour || 0, this.minute || 0, this.second || 0, this.millisecond || 0);
+        }
+    }
+    str() {
+        return this.strftime('%Y-%m-%d %H:%M:%S.%f');
+    }
+    valueOf() {
+        if (this.utc) {
+            return Date.UTC(this.year, this.month - 1, this.day || 1, this.hour || 0, this.minute || 0, this.second || 0, this.millisecond || 0);
+        }
+        else {
+            return this.jsDate.getTime();
+        }
+    }
+    toString() {
+        return this.str();
+    }
+    toJSON() {
+        return this.str();
+    }
+    strftime(format) {
+        if (this.utc) {
+            return d3TimeFormat.utcFormat(format)(this.jsDate);
+        }
+        else {
+            return d3TimeFormat.timeFormat(format)(this.jsDate);
+        }
+    }
+    time() {
+        return new PyTime(this.hour, this.minute, this.second, this.millisecond);
+    }
+    date() {
+        return new PyDate(this.year, this.month, this.day);
+    }
+    weekday() {
+        // javascript week starts on sunday, while python one starts on monday
+        return this.date().weekday();
+    }
+    isoweekday() {
+        return this.weekday() + 1;
+    }
+}
---
+++ dist/classes/timedelta.js
@@ -0,0 +1,54 @@
+import * as d3TimeFormat from 'd3-time-format';
+import { TimedeltaIntervals, toMillis, } from '../models';
+export class PyTimedelta {
+    constructor(days, seconds, milliseconds, minutes, hours, weeks) {
+        let args = {
+            days: days,
+            seconds,
+            milliseconds,
+            minutes,
+            hours,
+            weeks,
+        };
+        if (days != null && typeof days != 'number') {
+            // we have a dict
+            args = days;
+        }
+        else if (Math.abs(days) > 900) {
+            // we have millis, let's deconstruct into weeks, days, hours, minutes, seconds, milliseconds
+            let totalMillis = days ?? 0;
+            args = {};
+            TimedeltaIntervals.forEach((key) => {
+                const multiplier = toMillis[key];
+                const val = Math.floor(totalMillis / multiplier);
+                if (val) {
+                    args[key] = val;
+                    totalMillis -= val * multiplier;
+                }
+            });
+        }
+        TimedeltaIntervals.forEach((key) => {
+            this[key] = args[key] || 0;
+        });
+    }
+    get __totalMillis() {
+        let millis = TimedeltaIntervals.map((field) => this[field] *
+            toMillis[field]);
+        return millis.reduce((total, current) => total + current);
+    }
+    str() {
+        return d3TimeFormat.timeFormat('%H:%M:%S.%f')(new Date(this));
+    }
+    valueOf() {
+        return this.__totalMillis;
+    }
+    toString() {
+        return this.str();
+    }
+    toJSON() {
+        return this.str();
+    }
+    totalSeconds() {
+        return this.__totalMillis / 1000;
+    }
+}
