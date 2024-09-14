4e1ee7f941e802f2955374be5f3db73808df75c3
Nerwyn Singh <nerwyn1994@gmail.com>
2024-09-13 23:33:53 -0400

    try setting webpack ibrary type to commonjs-module and use that as the dist build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -14,7 +14,7 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, './dist'),
-		filename: '[name].min.js',
+		filename: '[name].js',
 		library: {
 			type: 'commonjs-module',
 			name: 'py-datetime',
